7fa8c3410d22afcae7f6292060728ca36c649cb4
David Lance <david6116@yahoo.com>
2020-07-09 16:05:23 -0400

    remove some log statements and clean up spacing a bit

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-console.log('hello')
-
-
 class App extends React.Component {
     state = {
         posts:[]
@@ -28,12 +25,9 @@ class App extends React.Component {
             }
         ).then(
             (response) => {
-                console.log(response);
-
                 this.setState({
                     posts:response.data
                 })
-                console.log(this.state.posts)
             }
         )
     }
@@ -42,7 +36,6 @@ class App extends React.Component {
         this.setState({
             newPostImage:event.target.value
         })
-        console.log(this)
     }
 
     changeNewPostName = (event) => {
@@ -59,27 +52,22 @@ class App extends React.Component {
 
     render = () => {
         return <div>
-
         <div className="header">
           <img src='https://i.postimg.cc/50FtCWhS/photo-of-daisy-flowers-1477166.jpg' className="img-fluid" alt="Responsive image"/>
         </div>
-
-
-
-
         <h1 className="mt-5">Inspirational Quote Generator</h1>
         <div className="card mt-4">
-        <h2 className= "ml-4 mr-4 mt-4 mb-4" >Create a quote</h2>
-        <form onSubmit={this.createPost}>
-          <div className="form-group ml-4 mr-4">
-            <input className="form-control" onKeyUp={this.changeNewPostName} type="text" placeholder="name" /><br/>
-            <input className="form-control" onKeyUp={this.changeNewPostPost} type="text" placeholder="text.." /><br/>
-            <input className="form-control" onKeyUp={this.changeNewPostImage} type="url" placeholder="http://" /><br/>
-            <input type="submit" value="Create post" />
+            <h2 className= "ml-4 mr-4 mt-4 mb-4" >Create a quote</h2>
+            <form onSubmit={this.createPost}>
+            <div className="form-group ml-4 mr-4">
+                <input className="form-control" onKeyUp={this.changeNewPostName} type="text" placeholder="name" /><br/>
+                <input className="form-control" onKeyUp={this.changeNewPostPost} type="text" placeholder="text.." /><br/>
+                <input className="form-control" onKeyUp={this.changeNewPostImage} type="url" placeholder="http://" /><br/>
+                <input type="submit" value="Create post" />
             </div>
             </form>
-            </div>
-            <h2 className="mt-4"> Quotes: </h2>
+        </div>
+        <h2 className="mt-4"> Quotes: </h2>
         <ul>
         {
             this.state.posts.map(
@@ -88,20 +76,17 @@ class App extends React.Component {
                         <div className="card text-white bg-dark text-center h-100">
                             <img className="card-img-top h-50" src={person.image} />
                             <div className="card-body">
-                            <h5 className="card-title">{person.name}</h5>
-                            <p className="card-text">{person.body}</p>
-                            <a href="#">
-                                <button type="button" name="button" className="btn btn-primary mb-4 mt-5 bg-info">Edit</button>
-                            </a>
-
-                            <form action="#" method="POST">
-                                <input className="btn btn-primary bg-info" type="submit" value="DELETE"/>
-                            </form>
-
+                                <h5 className="card-title">{person.name}</h5>
+                                <p className="card-text">{person.body}</p>
+                                <a href="#">
+                                    <button type="button" name="button" className="btn btn-primary mb-4 mt-5 bg-info">Edit</button>
+                                </a>
+                                <form action="#" method="POST">
+                                    <input className="btn btn-primary bg-info" type="submit" value="DELETE"/>
+                                </form>
+                            </div>
                         </div>
                     </div>
-
-                </div>
                 }
             )
         }

0d76b759488bade541cd6c977b60ac7600ed0a5f
David Lance <david6116@yahoo.com>
2020-07-09 12:50:29 -0400

    add some db config stuff

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,7 +1,10 @@
 <?php
-
 include_once __DIR__ . '/../models/post.php';
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 if ($_REQUEST['action'] === 'index') {
   echo json_encode(Posts::all());
 } elseif ($_REQUEST['action'] === 'post') {
@@ -16,9 +19,9 @@ if ($_REQUEST['action'] === 'index') {
   $updated_post = new Post($_REQUEST['id'], $body_object->name, $body_object->image, $body_object->body);
   $all_posts = Posts::update($updated_post);
   echo json_encode($all_posts);
-  } else if ($_REQUEST['action'] === 'delete') {
-    $all_posts = Posts::delete($_REQUEST['id']);
-    echo json_encode($all_posts);
-  }
+} else if ($_REQUEST['action'] === 'delete') {
+  $all_posts = Posts::delete($_REQUEST['id']);
+  echo json_encode($all_posts);
+}
 
- ?>
+?>
